fcab30f31708f6a181ca9690576ab4b39704fbfd
Y16Chryatis <y16.chryatis@gmail.com>
2024-10-03 00:38:54 -0600

    se creo ejemplo inload

---
+++ html-in-load/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    let htmlparth = `
+    <h1> SE agrega este html </h1>\n
+    <p>Mientras se carga el documento</p>`;
+
+    document.getElementById("lobby").innerHTML = htmlparth;
+
+  })
